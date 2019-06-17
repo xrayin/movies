69370c7834538d87f8004bd963d32ea95d69a736
xrayin <rai.ramzan@hotmail.com>
2019-06-17 05:07:45 +0200

    adjusted gitignore properly to not ignore the build folder and added the build folder to test on webserver Vimexx

---
+++ build/precache-manifest.dba5c254d3fd05320a80f95b9ae7624d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "204b597ac03333384825",
+    "url": "/static/css/main.ced58801.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "494c0ecdf32bf5e9b7e9",
+    "url": "/static/js/2.87cf8f5c.chunk.js"
+  },
+  {
+    "revision": "204b597ac03333384825",
+    "url": "/static/js/main.7c40ee74.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "494c0ecdf32bf5e9b7e9",
+    "url": "/static/css/2.b8447824.chunk.css"
+  },
+  {
+    "revision": "718f98c805e7d611c972fe9f1bb8b623",
+    "url": "/index.html"
+  }
+];
